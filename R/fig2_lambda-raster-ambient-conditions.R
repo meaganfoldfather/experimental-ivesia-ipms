f37baf615824bc7abc4e490f642fe13ab6767ee1
MeaganOldfather <meagan.f.oldfather@gmail.com>
2020-12-28 15:30:55 -0700

    Updated Figure 1 so that the site show the site-specific lambda values

--- R/fig2_lambda-raster-ambient-conditions.R
+++ R/fig2_lambda-raster-ambient-conditions.R
@@ -70,6 +70,33 @@ outline <- sf::st_as_sf(pp) %>% dplyr::filter(stable == 1)
 ### Get the site metadata
 site_metadata <- readr::read_csv(file = "data/data_output/site_metadata.csv")
 
+# bring in site lambda values
+### Get the site metadata
+site_lambdas<- readr::read_csv(file = "data/data_output/site_specific_additive.csv")
+site_lambdas
+dim(site_lambdas)
+
+ambient_site_lambdas <- 
+  site_lambdas %>% 
+  dplyr::left_join(site_metadata, by = "site") %>% 
+  dplyr::mutate(manipulated_vr = case_when(heat.f == 1 & heat.g == 1 & heat.s == 1 & water.f == 1 & water.g == 1 & water.s == 1 ~ "hw",
+                                           heat.f == 1 & heat.g == 1 & heat.s == 1 ~ "heat",
+                                           water.f == 1 & water.g == 1 & water.s == 1 ~ "water",
+                                           heat.f == 1 | water.f == 1 ~ "fecundity",
+                                           heat.g == 1 | water.g == 1 ~ "growth",
+                                           heat.s == 1 | water.s == 1 ~ "survivorship",
+                                           TRUE ~ "ambient")) %>% 
+  dplyr::filter(manipulated_vr == 'ambient') %>% 
+  group_by(site, degree.days, vwc, site.num) %>% 
+  tidybayes::median_hdci(lambda) %>% 
+  dplyr::mutate(sig_lambda = ifelse(test = .lower >= 1 | .upper <= 1,
+                                    yes = lambda,
+                                    no = NA))
+
+
+ambient_site_lambdas
+
+
 # Build the figure
 figure2 <-
   mcvr_lambda_summary %>%
@@ -88,8 +115,14 @@ figure2 <-
   ylab("Soil Moisture (scaled)") +
   theme(text = element_text(size=16)) +
   labs(fill = expression(lambda)) +
-  geom_point(data = site_metadata, aes(degree.days_scaled, vwc_scaled), cex = 2) +
-  geom_text_repel(data = site_metadata, aes(degree.days_scaled, vwc_scaled, label = site.num), cex = 5) +
+  geom_point(data = ambient_site_lambdas[is.na(ambient_site_lambdas$sig_lambda),], aes(degree.days, vwc, bg = lambda), pch =23, cex = 5) +
+    geom_point(data = ambient_site_lambdas[!is.na(ambient_site_lambdas$sig_lambda),], aes(degree.days, vwc, bg = lambda), pch =25, cex = 5) +
+  geom_text_repel(data = ambient_site_lambdas, aes(degree.days, vwc, label = site.num), cex = 5) +
+   scale_color_gradient2(mid = "grey90",
+                       midpoint = 1,
+                       na.value = "grey90",
+                       high = "darkgreen",
+                       low = "darkgoldenrod1")+
   geom_sf(data = outline, inherit.aes = FALSE, fill = NA, lwd = 1.5)
 
 figure2
